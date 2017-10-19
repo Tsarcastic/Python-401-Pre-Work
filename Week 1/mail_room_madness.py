ae2578c5d17d92e3c1e89c5198b20a97b8825a71
Brendan Davis <BrendanMD@gmail.com>
2017-10-19 16:39:59 -0700

    more

--- Week 1/mail_room_madness.py
+++ Week 1/mail_room_madness.py
@@ -1,22 +1,20 @@
-donors_amt = {'example': 100, 'other':200}
-donors_ct = {'example':1, 'other':2}
+donors_amt = {'Surname': 100, 'Cardholder':200}
+donors_ct = {'Surname':1, 'Cardholder':2}
 
 """Menu Interface"""
 def mail_room():
     reply = None
-    while reply is not quit:
+    while reply is not 3:
         print("Message")
         print("1: Process a Donation & Send a Thank You")
         print("2: Create a Report")
         print("3. Quit this program")
         print("(Enter 'q' at any time to return to this menu.)")
-        reply = int(input("1 or 2?")) #Input
+        reply = int(input("1, 2 or 3?")) #Input
         if reply == 1:
             add_amount()
         if reply == 2:
             report()
-        if reply == 3:
-            reply = quit
 
 """Adds the amount to the donor's total"""
 def add_amount():
@@ -29,23 +27,25 @@ def add_amount():
         if amount == "q":
             return
     if name in donors_amt:
-        donors_amt[name] += amount
+        donors_amt[name] += int(amount)
         donors_ct[name] += 1
     else: 
-        donors_amt[name] = amount
+        donors_amt[name] = int(amount)
         donors_ct[name] = 1
-    print_letter()
+    print_letter(name, amount)
 
 """This will print out the report"""   
 def report():
-    print("\tName\t\tTimes\tAmount\tAverage")
+    print(donors_amt)
+    print(donors_ct)
+    print("\tName\t\t\tTimes\tAmount\tAverage")
     for donor in donors_amt:
         print("\t{}\t\t{}\t${}\t${}".format(donor,donors_ct[donor], donors_amt[donor], donors_amt[donor]/donors_ct[donor] ))
 
-#def print_letter():
-    
-    #This is where we'll print the letter
-
+def print_letter(to, contribution):
+    print("""Dear Mr. or Mrs. {},
+    \nThank you very much for your generous donation of ${}. It's thanks to people like you that we are able to continue our noble cause of shaving cats. Without your generous contribution even more cats would be living on the streets with a full coat of hair
+    """.format(to, contribution))    
 
 mail_room()
     
